fc2950d8510c0b4b7effe851fd3d45ff7232c49b
Hardik Jain <hardik.jain@gfk.com>
2019-07-10 06:33:01 +0100

    pagination for list ,setup for commit history done

--- src/friends.jsx
+++ src/friends.jsx
@@ -13,63 +13,206 @@ const axiosGitHubGraphQL = axios.create({
 });
 
 const searchFriends = `
-  query SearchTop100Users($searchString: String!) {
-    search(query: $searchString, type: USER, first: 100) {
+  query SearchTop100Users($searchString: String!, $first: Int, $last: Int, $afterCursor: String, $beforeCursor: String) {
+    search(query: $searchString, type: USER, first: $first, last: $last, after: $afterCursor, before: $beforeCursor) {
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
       nodes {
-        __typename
         ... on User{
           login
           avatarUrl
+          id
         }
       }
     }
   }
 `;
 
+const commitHistory = `
+  query friendsCommitHistory($Author: CommitAuthor){
+    history(author: $Author, first: 5){
+
+    }
+  }
+`;
+
+const initialPageInfo = {startCursor:null, endCursor:null, hasNextPage:null, hasPreviousPage:null};
 class Friends extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
+      username: '',
+      tempuser: '',
+      search:false,
       matchedUsers: [],
-      olduser: this.props.username
+      id:'',
+      showCommit: false,
+      commit:[],
+      pageInfo: initialPageInfo,
+      first: 100,
+      last: null
     };
     this.onFetchFromGitHub = this.onFetchFromGitHub.bind(this);
+    this.onFetchFromGitHubCommit = this.onFetchFromGitHubCommit.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.list = this.list.bind(this);
   }
 
   onFetchFromGitHub() {
     axiosGitHubGraphQL.post('', {
       query: searchFriends,
       variables:{
-        searchString: this.props.username
+        searchString: this.state.username,
+        afterCursor: this.state.pageInfo.endCursor,
+        beforeCursor: this.state.pageInfo.startCursor,
+        first: this.state.first,
+        last: this.state.last
+      }
+    }).then(result =>{
+      const dataWithLogin = result.data.data.search.nodes.filter(user => user.login);
+      const filteredData = dataWithLogin.filter(user => user.login.toLowerCase().includes(this.state.username));
+      this.setState({
+        matchedUsers: filteredData,
+        showCommit: false,
+        search:false,
+        commit:[],
+        pageInfo: result.data.data.search.pageInfo
+      });
+    });
+  }
+
+  onFetchFromGitHubCommit() {
+    axiosGitHubGraphQL.post('', {
+      query: commitHistory,
+      variables:{
+        Author: {id : this.state.id}
       }
     }).then(result =>
       this.setState({
-        matchedUsers: result.data.data.search.nodes.filter(user => user.login.toLowerCase().includes(this.props.username)),
-        olduser: this.props.username
+        commit: result.data.errors, //.history.nodes,
+        olduser: this.state.username,
+        id: ''
       })
     );
   }
 
   componentDidUpdate() {
-    if (this.state.olduser !== this.props.username) {
+    if (this.state.search) {
       this.onFetchFromGitHub();
     }
+    if (this.state.id !== '') {
+      this.onFetchFromGitHubCommit();
+    }
+
   }
 
+  onChange(event) {
+    this.setState({
+      tempuser: event.target.value
+    });
+  }
+
+  onSubmit(event) {
+    this.setState({
+      username: this.state.tempuser,
+      first:100,
+      search:true,
+      last:null,
+      pageInfo:initialPageInfo,
+      matchedUsers: [],
+      id:'',
+      showCommit: false,
+      commit:[]
+    });
+    event.preventDefault();
+  }
+
+  onClick(event) {
+    if (event.currentTarget.dataset.id === 'next') {
+      if (this.state.pageInfo.hasNextPage) {
+        this.setState({
+          pageInfo:{
+            startCursor:null,
+            endCursor:this.state.pageInfo.endCursor,
+            hasNextPage:this.state.pageInfo.hasNextPage,
+            hasPreviousPage:this.state.pageInfo.hasPreviousPage
+          },
+          first:100,
+          last:null,
+          search: this.state.pageInfo.hasNextPage
+        });
+      }
+    } else if (event.currentTarget.dataset.id === 'prev') {
+      if (this.state.pageInfo.hasPreviousPage) {
+        this.setState({
+          pageInfo:{
+            startCursor:this.state.pageInfo.startCursor,
+            endCursor:null,
+            hasNextPage:this.state.pageInfo.hasNextPage,
+            hasPreviousPage:this.state.pageInfo.hasPreviousPage
+          },
+          first:null,
+          last:100,
+          search: this.state.pageInfo.hasPreviousPage
+        });
+      }
+    } else {
+      this.setState({
+        id: event.currentTarget.dataset.id,
+        showCommit: true,
+        pageInfo: initialPageInfo, first:100, last:null, search:false, commit:[]
+      });
+    }
+  }
+  list() {
+    if (!this.state.showCommit) {
+      return (this.state.matchedUsers.map(user => {
+        return (
+          <li data-id={user.id} onClick={this.onClick}>
+            <img src={user.avatarUrl} height="30" width="30"/> {user.login}
+          </li>
+        );
+      }));
+    }
+    return (this.state.commit.map(error => {
+      return (
+        <li>
+          {error.message}
+        </li>
+      );
+    }));
+  }
   render() {
+
     return (
-      <>
+      <div className={Style.something}>
+        <h1>Friends</h1>
+        <form onSubmit={this.onSubmit}>
+          <label>
+            Username:
+          </label>
+          <input
+            type="text"
+            onChange={this.onChange}
+            style={{width: '300px'}}
+          />
+          <button type="submit">Search</button>
+        </form>
+        <hr/>
+        <div data-id="next" onClick={this.onClick}>next</div>
+        <div data-id="prev" onClick={this.onClick}>prev</div>
         {
-          this.state.matchedUsers.map(user => {
-            return (
-              <div className={Style.something}>
-                <img src={user.avatarUrl} height="30" width="30"/> {user.login}
-              </div>
-            );
-          })
+          this.list()
         }
-      </>
+      </div>
     );
+
   }
 }
 
